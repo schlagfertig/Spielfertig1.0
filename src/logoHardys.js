81bd8a70755a3e6f2c0b7ae9ec6782a8f407aa28
schlagfertig <tom.o.schuster83@gmail.com>
2026-08-31 09:46:09 +0200

    Hardy's Original-Scan als Data-URI Teile P0–P3

--- src/logoHardys.js
+++ src/logoHardys.js
@@ -1,4 +1,5 @@
-import { A } from "./logoHardysA";
-import { B } from "./logoHardysB";
-import { C } from "./logoHardysC";
-export const LOGO_HARDYS_DATA = "data:image/jpeg;base64," + A + B + C;
+import { P0 } from "./logoHardysP0";
+import { P1 } from "./logoHardysP1";
+import { P2 } from "./logoHardysP2";
+import { P3 } from "./logoHardysP3";
+export const LOGO_HARDYS_DATA = "data:image/jpeg;base64," + P0 + P1 + P2 + P3;
