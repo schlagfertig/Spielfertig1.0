e37190b26ed1829dd29644803bae5b589d64b87b
schlagfertig <tom.o.schuster83@gmail.com>
2026-09-09 18:07:31 +0200

    SW v13 – Ansichts-Schalter

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // SPIELFERTIG‽ – Service Worker
-const CACHE = 'spielfertig-v12';
+const CACHE = 'spielfertig-v13';
 const PRECACHE = ['/', '/index.html', '/Logo-dark.png', '/Logo-light.png'];
 
 self.addEventListener('install', e => {
