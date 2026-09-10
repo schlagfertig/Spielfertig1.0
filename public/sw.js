915ea367fb76ed9752e1438f3c7e9ce63681bf1d
schlagfertig <tom.o.schuster83@gmail.com>
2026-09-10 10:10:36 +0200

    Bump SW cache to v28 so login picks up original seal

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // SPIELFERTIG – Service Worker
-const CACHE = 'spielfertig-v26';
-const PRECACHE = ['/', '/index.html', '/Logo-dark.png', '/Logo-light.png', '/Logo-full.svg'];
+const CACHE = 'spielfertig-v28';
+const PRECACHE = ['/', '/index.html', '/Logo-dark.png', '/Logo-light.png', '/Logo-full.svg', '/Logo-full.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
