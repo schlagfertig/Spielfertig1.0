4a757b61043b435299123f49b4e9c2caca9d97ea
schlagfertig <tom.o.schuster83@gmail.com>
2026-09-10 08:07:37 +0200

    SW v19 – schlankes Interrobang

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // SPIELFERTIG – Service Worker
-const CACHE = 'spielfertig-v18';
+const CACHE = 'spielfertig-v19';
 const PRECACHE = ['/', '/index.html', '/Logo-dark.png', '/Logo-light.png'];
 
 self.addEventListener('install', e => {
