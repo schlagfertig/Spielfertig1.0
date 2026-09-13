33798a9024749f76b967ae472be1d714876c8409
schlagfertig <tom.o.schuster83@gmail.com>
2026-09-13 20:24:01 +0200

    Cache v41 – Mehrfachauswahl für Sets.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // SPIELFERTIG – Service Worker
-const CACHE = 'spielfertig-v40';
+const CACHE = 'spielfertig-v41';
 const PRECACHE = ['/', '/index.html', '/Logo-dark.png', '/Logo-light.png'];
 
 self.addEventListener('install', e => {
