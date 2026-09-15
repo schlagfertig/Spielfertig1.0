6d4ba3558c2db87ede05d39cf0756300d3d21c82
schlagfertig <tom.o.schuster83@gmail.com>
2026-09-15 10:35:50 +0200

    Setdauer: Song-Feld plus Summen in Playlist, Gig und Share.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // SPIELFERTIG – Service Worker
-const CACHE = 'spielfertig-v43';
+const CACHE = 'spielfertig-v44';
 const PRECACHE = ['/', '/index.html', '/Logo-dark.png', '/Logo-light.png'];
 
 self.addEventListener('install', e => {
